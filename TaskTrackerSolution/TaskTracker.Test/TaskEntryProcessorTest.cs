e469514a27d86299364969b1e59e1cfc4b54990c
eisakarate <87922527+eisakarate@users.noreply.github.com>
2025-03-25 05:48:00 +0000

    Adding Additional Unit Tests

--- TaskTrackerSolution/TaskTracker.Test/TaskEntryProcessorTest.cs
+++ TaskTrackerSolution/TaskTracker.Test/TaskEntryProcessorTest.cs
@@ -39,4 +39,76 @@ public class TasKEntryProcessorTest
         if(userModelToSave.TaskEntries[0].EntryId != 1)
             Assert.Fail("Entry Id is not 1");
     }
+    
+    [Fact]
+    public void TestAddTwoNewToEmptyUserModel()
+    {
+        var userModelToSave = new UserModel(){ UserName = "test"};
+        //add one
+        userModelToSave.AddTask(
+        title: "testing", 
+        status: TaskEntryStatus.New, 
+        priority: TaskEntryPriority.Low, 
+        dueDate: DateTime.Now,
+        description: "Test", 
+        project: "Unit Test", 
+        material: "Cheese");
+        //add 2
+        userModelToSave.AddTask(
+        title: "testing2", 
+        status: TaskEntryStatus.New, 
+        priority: TaskEntryPriority.Low, 
+        dueDate: DateTime.Now,
+        description: "Test2", 
+        project: "Unit Test2", 
+        material: "Cheese2");
+
+
+        //check if item is added
+        if(userModelToSave.TaskEntries.Count == 0)
+            Assert.Fail("No items added");
+        //check if item count is 1
+        if(userModelToSave.TaskEntries.Count != 2)
+            Assert.Fail($"Expected 2 items got: {userModelToSave.TaskEntries.Count}");
+        //check if the entry's EntryId is 1
+        if(userModelToSave.TaskEntries[0].EntryId != 1)
+            Assert.Fail("Entry Id is not 1");
+        //Check if the second entry id is 2
+        if(userModelToSave.TaskEntries[1].EntryId != 2)
+            Assert.Fail("Entry Id is not 2");
+    }    
+
+    [Fact]
+    public void TestDuplicateTitleCheck()
+    {
+        var userModelToSave = new UserModel(){ UserName = "test"};
+        try
+        {
+            //add one
+            userModelToSave.AddTask(
+            title: "testing", 
+            status: TaskEntryStatus.New, 
+            priority: TaskEntryPriority.Low, 
+            dueDate: DateTime.Now,
+            description: "Test", 
+            project: "Unit Test", 
+            material: "Cheese");
+            //add 2
+            userModelToSave.AddTask(
+            title: "testing", 
+            status: TaskEntryStatus.New, 
+            priority: TaskEntryPriority.Low, 
+            dueDate: DateTime.Now,
+            description: "Test2", 
+            project: "Unit Test2", 
+            material: "Cheese2");
+
+            //Error
+            Assert.Fail("Should not be able to add task with the same title");
+        }
+        catch(InvalidOperationException ex)
+        {
+            Assert.Equal("Title is already registered", ex.Message);
+        }
+    }
 }
